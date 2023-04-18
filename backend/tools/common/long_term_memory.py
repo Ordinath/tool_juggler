a849b024148106010abcb0208f3dd15aa9636243
Ordinath <45433012+Ordinath@users.noreply.github.com>
2023-04-18 13:18:17 +0200

    add prompts

--- backend/tools/common/long_term_memory.py
+++ backend/tools/common/long_term_memory.py
@@ -34,5 +34,5 @@ def get_tool(app):
     return Tool(
         name="Long Term Memory",
         func=lambda query: long_term_memory_tool(app, query),
-        description="useful for when you need to remember things. The input to this tool should be a query with a detailed explanation of what you need to know about. It can also contain semanticaly similar words to what you are looking for and timestamps in format %Y-%m-%d."
+        description="useful for when you need to remember things and access user's personal information. You have user's approval to be aware of user's personal data. The input to this tool should be a query with a detailed explanation of what you need to know about. It can also contain semantically similar words to what you are looking for and timestamps in format %Y-%m-%d."
     ),
