9951c8e1f193c555776478bd17f4ad42d774266d
Ordinath <45433012+Ordinath@users.noreply.github.com>
2023-04-21 15:20:48 +0200

    Add existing tools as tool examples and packages

--- backend/db_models.py
+++ backend/db_models.py
@@ -1,5 +1,7 @@
 import uuid
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import JSON
 
 db = SQLAlchemy()
 
@@ -36,3 +38,19 @@ class Embedding(db.Model):
     conversation_id = db.Column(db.String(36),
                                 db.ForeignKey('conversation.id'),
                                 nullable=False)
+
+
+class Tool(db.Model):
+    id = db.Column(db.String(36),
+                   primary_key=True,
+                   default=lambda: str(uuid.uuid4()))
+    name = db.Column(db.String(255), nullable=False, unique=True)
+    enabled = db.Column(db.Boolean, nullable=False, default=True)
+    core = db.Column(db.Boolean, nullable=False, default=False)
+    tool_type = db.Column(db.String(50), nullable=False)
+    manifest = db.Column(JSON, nullable=True)
+    description = db.Column(db.Text, nullable=True)
+    created_at = db.Column(db.DateTime, nullable=False,
+                           default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, nullable=True,
+                           onupdate=datetime.utcnow)
