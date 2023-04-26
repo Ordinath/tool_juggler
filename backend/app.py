54c9c870b8bc4cebd81334d828664b3bfb0b42b2
Ordinath <45433012+Ordinath@users.noreply.github.com>
2023-04-26 19:14:56 +0200

    Add tool upserting support

--- backend/app.py
+++ backend/app.py
@@ -31,4 +31,6 @@ register_routes(app)
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=5005)
+    app.run(debug=True, port=5005, use_reloader=False)
+
+    
